be0c24484f2d6a9f16025fca425369ca1c694cbd
MD-AHAD-KHAN-PATHAN <md.ahadkhanpathan.tct@gmail.com>
2024-01-01 23:59:16 +0600

    latest news card added

--- src/components/ui/LatestNews/LatestNews.js
+++ src/components/ui/LatestNews/LatestNews.js
@@ -1,4 +1,4 @@
-import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
+import { Box, Button, Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
 import Image from "next/image";
 import bitcoin from "@/assets/topnews.png"
 
@@ -9,15 +9,18 @@ const LatestNews = () => {
             <Card>
                 <CardActionArea>
                     <CardMedia>
-                        <Image src={bitcoin} width={800} alt="top news"/>
+                        <Image src={bitcoin} width={800} alt="top news" />
                     </CardMedia>
                     <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Lizard
+                        <p className="my-4 w-[120px] bg-red-600 text-white font-bold text-center py-1 rounded-sm">Technology</p>
+                        <Typography gutterBottom variant="h4" component="div">
+                            Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" className="my-2">
+                            By Ahad Khan - Mar 18 2023
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout........
                         </Typography>
                     </CardContent>
                 </CardActionArea>
