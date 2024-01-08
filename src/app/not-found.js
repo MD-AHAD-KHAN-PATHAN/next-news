285a5031cd01498757bbe7c434efae985d8d534c
MD-AHAD-KHAN-PATHAN <md.ahadkhanpathan.tct@gmail.com>
2024-01-08 23:08:46 +0600

    error page added

---
+++ src/app/not-found.js
@@ -0,0 +1,13 @@
+
+const NoteFoundPage = () => {
+    return (
+        <div className="flex justify-center items-center min-h-[60vh] text-center">
+            <div>
+                <h1 className="text-5xl font-bold text-red-600">404 !</h1>
+                <p className="font-bold">Page Not Found !!!</p>
+            </div>
+        </div>
+    );
+};
+
+export default NoteFoundPage;
