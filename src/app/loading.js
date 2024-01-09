f1c289eadca089bda89dd5d7f1ecdec02542a8b5
MD-AHAD-KHAN-PATHAN <md.ahadkhanpathan.tct@gmail.com>
2024-01-09 22:21:08 +0600

    loading page added

---
+++ src/app/loading.js
@@ -0,0 +1,13 @@
+
+const LoadingPage = () => {
+
+    return (
+        <div className="flex justify-center items-center min-h-[50vh] text-center">
+            <div>
+                <p className="font-bold text-5xl">Loading.......</p>
+            </div>
+        </div>
+    )
+}
+
+export default LoadingPage;
